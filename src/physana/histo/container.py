9c2404120d9a8b2fcdbfc5a02e97d61c15bc25c9
neko-0 <yuzhan.physics@gmail.com>
2025-04-27 14:31:10 -0400

    refactor: Added type hinting to (Base)AnalysisContainer

--- src/physana/histo/container.py
+++ src/physana/histo/container.py
@@ -1,5 +1,6 @@
 from sys import getsizeof
 from copy import copy, deepcopy
+from typing import Dict, Any, Optional, Union
 
 from .tools import get_all_slots, to_numexpr
 
@@ -14,43 +15,45 @@ class BaseAnalysisContainer:
         "_metadata_deepcopy_exclusions",
     )
 
-    def __init__(self, name):
+    def __init__(self, name: str):
         self._name = name
-        self.parent = None
-        self.metadata = {}
-        self._data = {}
-        self._deepcopy_exclusions = {
+        self.parent: Optional[BaseAnalysisContainer] = None
+        self.metadata: Dict[str, Any] = {}
+        self._data: Dict[str, Any] = {}
+        self._deepcopy_exclusions: set = {
             "parent",
             "_deepcopy_exclusions",
             "_metadata_deepcopy_exclusions",
         }
-        self._metadata_deepcopy_exclusions = set()
+        self._metadata_deepcopy_exclusions: set = set()
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{type(self).__name__}:{self.name}"
 
-    def __iter__(self):
+    def __iter__(self) -> iter:
         return iter(self._data.values())
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: str) -> Any:
         try:
             return self._data[key]
         except KeyError:
             raise KeyError(f"{key} not found. Possible keys: {', '.join(self._data)}")
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: str, value: Any) -> None:
         self._data[key] = value
 
-    def __contains__(self, key):
+    def __contains__(self, key: str) -> bool:
         return key in self._data
 
-    def __copy__(self):
+    def __copy__(self) -> "BaseAnalysisContainer":
         return self._copy()
 
-    def __deepcopy__(self, memo):
+    def __deepcopy__(
+        self, memo: Dict[int, "BaseAnalysisContainer"]
+    ) -> "BaseAnalysisContainer":
         return self._copy(memo)
 
-    def __sizeof__(self):
+    def __sizeof__(self) -> int:
         total_size = 0
         for attribute in get_all_slots(self):
             if attribute == "parent":
@@ -60,7 +63,7 @@ class BaseAnalysisContainer:
             total_size += getsizeof(data_item)
         return total_size
 
-    def _copy(self, memo=None):
+    def _copy(self, memo: Optional[Dict[int, Any]] = None) -> "BaseAnalysisContainer":
         """
         Make a copy of this container.
 
@@ -124,48 +127,50 @@ class BaseAnalysisContainer:
 
         return copied_self
 
-    def copy(self, *, shallow=False, no_parent=False):
+    def copy(
+        self, *, shallow: bool = False, no_parent: bool = False
+    ) -> "BaseAnalysisContainer":
         copied_instance = copy(self) if shallow else deepcopy(self)
         if no_parent:
             copied_instance.parent = None
         return copied_instance
 
-    def items(self):
+    def items(self) -> iter:
         return self._data.items()
 
-    def keys(self):
+    def keys(self) -> iter:
         return self._data.keys()
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self._name
 
     @name.setter
-    def name(self, rhs):
+    def name(self, rhs: str) -> None:
         if isinstance(self.parent, BaseAnalysisContainer):
             parent_data = self.parent._data
             parent_data[rhs] = parent_data.pop(self.name)
         self._name = rhs
 
     @property
-    def deepcopy_exclusions(self):
+    def deepcopy_exclusions(self) -> set:
         return self._deepcopy_exclusions
 
     @property
-    def deepcopy_metadata_exclusions(self):
+    def deepcopy_metadata_exclusions(self) -> set:
         return self._metadata_deepcopy_exclusions
 
     @property
-    def full_name(self):
+    def full_name(self) -> str:
         if self.parent is None:
             return self.name
         else:
             return f"{self.parent.full_name}/{self.name}"
 
-    def add_deepcopy_exclusion(self, name):
+    def add_deepcopy_exclusion(self, name: str) -> None:
         self._deepcopy_exclusions.add(name)
 
-    def discard_deepcopy_exclusion(self, name):
+    def discard_deepcopy_exclusion(self, name: str) -> None:
         if name not in {
             "parent",
             "_deepcopy_exclusions",
@@ -173,50 +178,49 @@ class BaseAnalysisContainer:
         }:
             self._deepcopy_exclusions.discard(name)
 
-    def add_metadata_deepcopy_exclusion(self, name):
+    def add_metadata_deepcopy_exclusion(self, name: str) -> None:
         self._metadata_deepcopy_exclusions.add(name)
 
-    def discard_metadata_deepcopy_exclusion(self, name):
+    def discard_metadata_deepcopy_exclusion(self, name: str) -> None:
         self._metadata_deepcopy_exclusions.discard(name)
 
-    def replace_data(self, other):
+    def replace_data(self, other: "BaseAnalysisContainer") -> None:
         self._data = other._data
         self.update_children_parent()
 
-    def clear(self):
+    def clear(self) -> None:
         self._data = {}
         self.metadata = {}
 
-    def clear_content(self, *args, **kwargs):
+    def clear_content(self, *args, **kwargs) -> None:
         raise NotImplementedError
 
-    def append(self, *args, **kwargs):
+    def append(self, *args, **kwargs) -> None:
         raise NotImplementedError
 
-    def update(self, *args, **kwargs):
+    def update(self, *args, **kwargs) -> None:
         raise NotImplementedError
 
-    def get(self, *args, **kwargs):
+    def get(self, *args, **kwargs) -> Any:
         raise NotImplementedError
 
-    def remove(self, *args, **kwargs):
+    def remove(self, *args, **kwargs) -> None:
         raise NotImplementedError
 
-    def update_children_parent(self, *args, **kwargs):
+    def update_children_parent(self, *args, **kwargs) -> None:
         pass
 
-    def clear_children_parent(self, *args, **kwargs):
+    def clear_children_parent(self, *args, **kwargs) -> None:
         raise NotImplementedError
 
-    def root_parent(self):
-        parent = self.parent
+    def root_parent(self) -> Optional["BaseAnalysisContainer"]:
+        parent: Optional[BaseAnalysisContainer] = self.parent
         while parent and parent.parent:
             parent = parent.parent
         return parent
 
 
 class AnalysisContainer(BaseAnalysisContainer):
-
     __slots__ = (
         "dtype",
         "weights",
@@ -225,23 +229,32 @@ class AnalysisContainer(BaseAnalysisContainer):
         "_selection",
     )
 
-    def __init__(self, name, weights=None, selection=None, dtype=None):
+    def __init__(
+        self,
+        name: str,
+        weights: Optional[str] = None,
+        selection: Optional[str] = None,
+        dtype: Optional[str] = None,
+    ) -> None:
         super().__init__(name)
-        self.dtype = dtype
-        self.weights = weights
-        self.cstyle_selection = selection
-        self._selection = to_numexpr(selection)
-        self.ntuple_branches = set()
+        self.dtype: Optional[str] = dtype
+        self.weights: Optional[str] = weights
+        self.cstyle_selection: Optional[str] = selection
+        self._selection: str = to_numexpr(selection)
+        self.ntuple_branches: set = set()
 
     @property
-    def selection(self):
+    def selection(self) -> str:
         return self._selection
 
     @selection.setter
-    def selection(self, value):
-        self.cstyle_selection = value
+    def selection(self, value: Union[str, List[str]]) -> None:
+        if isinstance(value, list):
+            self.cstyle_selection = " && ".join(value)
+        else:
+            self.cstyle_selection = value
         self._selection = to_numexpr(value)
 
-    def add_selection(self, rhs):
+    def add_selection(self, rhs: str) -> None:
         new_selection = " && ".join([self.cstyle_selection, rhs])
         self.selection = new_selection
