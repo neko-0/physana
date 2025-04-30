3bcebdebb48b8ab53473be5a8d79495e1ed16d25
neko-0 <yuzhan.physics@gmail.com>
2025-04-30 13:41:29 -0400

    fix:missing files from MR-4

--- src/physana/algorithm/__init__.py
+++ src/physana/algorithm/__init__.py
@@ -3,6 +3,7 @@ from .histmaker import HistMaker
 from .interface import run_algorithm, run_HistMaker
 from .sum_weights import extract_cutbook_sum_weights
 from .cutflow_reader import CutFlowReader
+from .prefilter_histo import PrefilterHistReader
 
 __all__ = [
     "BaseAlgorithm",
@@ -11,4 +12,5 @@ __all__ = [
     "run_HistMaker",
     "extract_cutbook_sum_weights",
     "CutFlowReader",
+    "PrefilterHistReader",
 ]
