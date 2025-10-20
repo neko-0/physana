297e90356f65b6938379370d23e45675ba5b1b0e
neko-0 <yuzhan.physics@gmail.com>
2025-10-20 14:55:22 -0400

    feat: update with metadata DB/

--- src/physana/tools/__init__.py
+++ src/physana/tools/__init__.py
@@ -1,5 +1,6 @@
 from .xsec import PMGXsec
 from .file_metadata import FileMetaData
+from .file_db import FileSQLiteDB, generate_metadata_db
 from .data_file_check import check_data_completeness
 from .sum_weights import SumWeightTool, extract_cutbook_sum_weights
 from .skim import SkimConfig
@@ -7,6 +8,8 @@ from .skim import SkimConfig
 __all__ = [
     'PMGXsec',
     'FileMetaData',
+    'FileSQLiteDB',
+    'generate_metadata_db',
     'check_data_completeness',
     'SumWeightTool',
     'extract_cutbook_sum_weights',
